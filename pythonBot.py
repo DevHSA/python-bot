c13a6eb46d1f7d600eb5f5488846e96d5926e4b4
DevHSA <harishsa85@gmail.com>
2021-03-20 06:53:56 +0530

    Stuff Added

--- pythonBot.py
+++ pythonBot.py
@@ -18,24 +18,33 @@ def click(x,y):
 
 while 1:
 
-    time.sleep(0.1)
-    win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL, 0, 0, win32con.WHEEL_DELTA * -1, 10)
-    time.sleep(0.1)
+    
     attack = pyautogui.locateOnScreen('attack.png', confidence=0.9)
     cont = pyautogui.locateOnScreen('continue.png', confidence=0.9)
     rebattle = pyautogui.locateOnScreen('rebattle.png', confidence=0.95)
+    select = pyautogui.locateOnScreen('select.png', confidence=0.95)
 
     if attack != None:
         print(attack)
+        time.sleep(0.01)
         click(attack[0]+10, attack[1]+10)
-        time.sleep(0.1)
+        
 
     if cont != None:
         print(cont)
+        time.sleep(0.1)
         click(cont[0]+10, cont[1]+10)
-        time.sleep(0.2)
+        
 
     if rebattle != None:
         print(rebattle)
+        time.sleep(0.1)
         click(rebattle[0]+5, rebattle[1]+5)
-        time.sleep(0.2)
+        
+
+    if select !=None:
+        print(select)
+        win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL, 0, 0, win32con.WHEEL_DELTA * -3, 10)
+        time.sleep(0.1)
+        
+        
---
+++ testScreen.py
@@ -0,0 +1,4 @@
+import pyautogui
+
+img = pyautogui.screenshot(region=(605,405,150,28))
+img.save(r"D:\GIT\python-bot\testimage.png")
