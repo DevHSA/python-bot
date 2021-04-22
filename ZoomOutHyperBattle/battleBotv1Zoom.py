ce4ff7bd1eba754fb334dd985bb1501feb7f49c0
DevHSA <harishsa85@gmail.com>
2021-04-22 17:06:40 +0530

    Further Perfected

--- ZoomOutHyperBattle/battleBotv1Zoom.py
+++ ZoomOutHyperBattle/battleBotv1Zoom.py
@@ -10,6 +10,7 @@ time.sleep(2)
 
 battles = 1
 counter = 0
+refreshCounter = 0
 battleFlag = 0
 clickMutex = 0
 
@@ -32,6 +33,7 @@ def click(x,y):
 while battles < 3000:
 
  counter = counter + 1
+ refreshCounter = refreshCounter + 1
  attack = pyautogui.locateOnScreen('./attack.png', region=(194,545,366,1009), grayscale=True, confidence=0.85)
  cont = pyautogui.locateOnScreen('./continue.png', region=(194,545,366,1009), grayscale=True, confidence=0.85)
 
@@ -45,6 +47,7 @@ while battles < 3000:
         #time.sleep(0.01)
         #print(attack)
         counter = 0
+        refreshCounter = 0
         battleFlag = 0
         click(attack[0]+randint(15,20), attack[1]+randint(14,17))
         #win32api.SetCursorPos((attack[0]+20,attack[1]+17))
@@ -58,11 +61,12 @@ while battles < 3000:
         #print(cont)
         #time.sleep(0.01)
         counter = 0
+        refreshCounter = 0
         battleFlag = 0
         click(cont[0]+10, cont[1]+14)
  else:
     
-    rebattle = pyautogui.locateOnScreen('./rebattle.png', region=(207,300,83,14), grayscale=True, confidence=0.85)
+    rebattle = pyautogui.locateOnScreen('./rebattle.png', region=(207,300,83,14), grayscale=True, confidence=0.95)
     
     if rebattle != None:
         #print("rebattle")
@@ -73,12 +77,19 @@ while battles < 3000:
             print(battles)
             battleFlag = 1
         counter = 0
+        refreshCounter = 0
         click(rebattle[0]+5, rebattle[1]+5)
 
 
- if counter > 10:
+ if counter > 6:
+     print("counter", counter)
      time.sleep(0.5)
-     #pyautogui.click()
-     pyautogui.press('f5')
-     time.sleep(4)
+     pyautogui.click()
+     time.sleep(0.5)
      counter = 0
+ if refreshCounter > 30:
+     print("refresh", refreshCounter)
+     time.sleep(0.5)
+     pyautogui.press('f5')
+     time.sleep(5)
+     refreshCounter = 0
